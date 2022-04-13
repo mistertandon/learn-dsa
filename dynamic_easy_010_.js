7fe2bfeccee328ae61515d4b0784db23b48fd0c9
mistertandon <enggparveshtandon@gmail.com>
2022-04-13 18:42:08 +0530

    fix: Delete unnecessary comments.

--- dynamic_easy_010_.js
+++ dynamic_easy_010_.js
@@ -2,15 +2,19 @@ let triangle = [[5], [1, 6], [4, 3, 10], [3, 2, 4, 1]];
 
 let rows = triangle.length;
 
+/**
+ * Traverse from 2nd last row to 0th row.
+ *    Traverse from 0th column of current to last column.
+ */
 for (let i = rows - 2; i >= 0; i--) {
   let cols = triangle[i].length;
   for (let j = 0; j < cols; j++) {
-    // console.log('i',i);
-    // console.log('j',triangle[i][j]);
-
     let left_sum = triangle[i][j] + triangle[i + 1][j];
     let right_sum = triangle[i][j] + triangle[i + 1][j + 1];
 
+    /**
+     *  min_sum: Minimum sub will replace previous value at `[i][j]` index
+     */
     let min_sum = left_sum <= right_sum ? left_sum : right_sum;
     triangle[i][j] = min_sum;
   }
